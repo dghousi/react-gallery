0e2cf77ab5aa6f65da12285696f34fa48aedfb9a
wmz786 <waismalikzada9@gmail.com>
2022-01-17 12:56:43 +0430

    deleted i

--- src/components/image/image.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from './images/1.jpg';
-
-export const Image = props =>{
-    return <div>
-        <img alt='photos' src={logo} width='100%'/>
-    </div>
-
-}
