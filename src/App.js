eab5bfbe556acfd82d250b216ff5422ba20af4e2
wmz786 <waismalikzada9@gmail.com>
2022-01-17 12:50:30 +0430

    imported the images to app and used it in state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import { Component} from 'react';
+import logo1 from './images/earthspring (12).jpg';
+import logo2 from './images/earthspring (13).jpg';
+import logo3 from './images/earthspring (15).jpg';
+import logo4 from './images/earthspring (5).jpg';
+import logo5 from './images/earthspring (6).jpg';
+import logo6 from './images/earthspring (7).jpg';
+import logo7 from './images/earthspring (1).jpg';
+import logo8 from './images/earthspring (9).jpg';
+import logo9 from './images/earthspring (11).jpg';
 
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
@@ -11,40 +20,60 @@ class App extends Component {
     this.state = {
       places: [
         {
+          image:logo1,
           name : 'Kabul',
           id : 1,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
         {
+          image:logo2,
           name : 'Herat',
           id : 2,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
         {
+          image:logo3,
           name : 'MazarESharef ',
           id : 3,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
         {
+          image:logo4,
           name : 'Kandahar',
           id : 4,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
         {
+          image:logo5,
           name : 'Kondoz',
           id : 5,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
         {
+          image:logo6,
           name : 'Badakhshan',
           id : 6,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
         {
+          image:logo7,
           name : 'Bamyan',
           id : 7,
           description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
         },
+        {
+          image:logo8,
+          name : 'Ghazne',
+          id : 8,
+          description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
+        },
+        {
+          image:logo9,
+          name : 'Norestan',
+          id : 9,
+          description : 'Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla '
+        },
+
       ],
       searchField: ""
     }
