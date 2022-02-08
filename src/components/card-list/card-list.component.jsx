65026dafee3b817dc4a8f386d7c104332295744d
wmz786 <waismalikzada9@gmail.com>
2022-02-08 23:55:19 +0430

    implement css in js with styled-components package and delete the reguller css styles

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import './card-list.style.css';
+// import './card-list.style.css';
 
 import {Card} from "../card/card.component";
+import { CardListContainer } from "./card-list.styles";
+
 
 export const CardList = props => {
-  return <div className='card-list'>
+  return <CardListContainer>
     {  props.places.map(place => 
       (<Card key={place.id} place={place}/>))}
-  </div>
+  </CardListContainer>
 };
